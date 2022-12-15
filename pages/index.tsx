d2fa8e54d03bb97efcc086efc7da88088728c312
Madhurya Putrevu <madhuryaputrevu28@gmail.com>
2022-12-15 13:41:40 -0500

    explored threejs!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,50 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import * as THREE from 'three'
+import { useRef } from 'react'
+import { Canvas, useThree, extend, useFrame } from '@react-three/fiber'
 
-const Home: NextPage = () => {
+extend({ Box: THREE.BoxGeometry })
+
+function SpinningCube() {
+  const mesh = useRef<THREE.Mesh>(null)
+
+  var increase = true;
+
+  useFrame(() => {
+      if (mesh.current) {
+        mesh.current.rotation.x += 0.01
+        mesh.current.rotation.y += 0.01
+
+        if (increase) {
+          mesh.current.scale.x += 0.01
+          if (mesh.current.scale.x >= 1) {
+            increase = false;
+          }
+        } else {
+          mesh.current.scale.x -= 0.01
+          if (mesh.current.scale.x <= 0.2) {
+            increase = true;
+          }
+        }
+      }
+  })
   return (
-    <div className={`bg-lime-500`}>Hello, World! 123</div>
+    <mesh ref={mesh}>
+      <boxGeometry attach="geometry" args={[3, 3, 3]} />
+      <meshStandardMaterial attach="material" color="rgb(216,180,254)" />
+    </mesh>
   )
 }
 
-export default Home
+function App() {
+  return (
+    <div className={`w-full h-screen`}>
+      <Canvas flat linear>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <SpinningCube />
+      </Canvas>
+    </div>
+  )
+}
+
+export default App;
